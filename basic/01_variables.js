ab6337d2f5af9dbd26d4422531e7050ce921d3e6
Sanskar Kaushal <oksanskarr@gmail.com>
2026-03-09 13:48:15 +0000

    container config done

---
+++ basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345;
+
+let accountEmail = "sankgla@gamail.com";
+
+var accountPass ="12455";
+
+accountCity = "indore";
+
+
+accountEmail ="hsfld@dflkfj";
+accountPass="password";
+accountCity="Pune";
+
+// console.log(accountEmail);
+// console.log(accountPass);
+// console.log(accountCity);
+// accountId = 2; 
+
+console.table([accountEmail,accountPass,accountCity])
+console.log(accountId);
