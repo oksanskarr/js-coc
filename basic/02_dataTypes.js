49d06bd76c58d215b4d395149fd1a7670cc5a4c8
Sanskar Kaushal <oksanskarr@gmail.com>
2026-03-09 14:28:17 +0000

    dataTypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+   "use strict"
+ // treat all js code as newer version Automatically works as stricts 
+// earlier in js classes and objects was not availble 
+
+// alert(3+3);
+// window.alert() instructs the browser to display a dialog with an optional 
+// message, and to wait until the user dismisses the dialog.
+
+console.log(3+3);
+
+let name = "jerry";
+let age = 25;
+let isLoggedIn = false;
+
+// Primitive DataTypes : 
+
+// number = 2 to the power 52
+// BigInt
+// String = ""
+// Boolean = true / false 
+// null = Stand alone value
+// undefined = 
+// Symbol = For Uniqueness
+// Object
+
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
