13f3f1b76ed33aafebb6921a057ebc65a2a08c7e
Sanskar Kaushal <oksanskarr@gmail.com>
2026-03-10 08:59:15 +0000

    conversionOperations

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = 33;
+let mark = "21";
+
+
+console.log(typeof score);
+console.log(typeof mark);
+
+// let scoreInString = String(score)
+// console.log = (typeof scoreInString);
+// console.log = (scoreInString);
+
+let markInNumber = Number(mark); // Type conversion 
+console.log(typeof markInNumber);
+console.log(markInNumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1 false = 0 
+
+let isLoggedIn = "";  //22
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" = false Empty String
+// "name"= true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
