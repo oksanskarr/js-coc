ab37860d23abf2045a8c8a270c0170725f0aaaec
Sanskar Kaushal <oksanskarr@gmail.com>
2026-03-10 11:23:46 +0000

    comparison

---
+++ basic/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);  //it is not recomanded to compare two diffrent Datatype
+
+console.log("--------------");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+console.log("--------------");
+
+// === it strictly checks the data type 
+
+console.log("2" === 2);
+
+
+
